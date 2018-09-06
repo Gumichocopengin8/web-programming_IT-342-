4789b5d56677a76d9c171b3f80470992b79d590d
keita_nonaka <nonakkx16@juniata.edu>
2018-09-06 18:49:54 +0000

    done with the array.php

--- arrays.php
+++ arrays.php
@@ -6,17 +6,23 @@
   </head>
 
   <body>
-
     <?php
       $serialNumber = ["C02P20DNG3NN", "FSVMJ72", "G5MNJ72", "D25P205ZF8J9", "2X7NJ72", "C02RR5JUG8WN", "HLDKH72", "58D8H72", "D25P205GF8J9", "C1MK8GZHDTY4"];
-      $manufactuer = ["Apple", "Dell", "Dell", "Apple", "Dell", "Apple", "Dell","Dell", "Apple", "Apple"];
+      $manufacturer = ["Apple", "Dell", "Dell", "Apple", "Dell", "Apple", "Dell","Dell", "Apple", "Apple"];
       $model = ["15\" MacBook Pro", "Latitude E7470", "Latitude E7470", "27\" iMac", "Latitude E5570", "15\" MacBook Pro",  "Latitude E5570",  "Latitude E5570", "27\" iMac", "13\" MacBook Pro"];
       $purchaseYear = ["2015", "2016", "2016", "2015", "2016", "2015", "2016", "2016", "2015", "2013"];
       $laptop = ["Yes", "Yes", "Yes", "", "Yes", "Yes", "Yes", "Yes", "", "Yes"];
 
-      print(count($serialNumber))
-    ?>
+      $col = count($serialNumber);
+      $appleCount = $dellCount = 0;
+      $comp = [];
 
+      for($i = 0; $i < $col; $i++){
+        $comp[] = ["serialNumber" => $serialNumber[$i], "manufacturer" => $manufacturer[$i],
+                   "model" => $model[$i], "purhaseYear" => $purchaseYear[$i], "laptop" => $laptop[$i], ];
+        $comp[$i]["manufacturer"] == "Apple" ? $appleCount++ : $dellCount++;
+      }
+    ?>
     <table border="1">
       <thead>
        <tr>
@@ -28,25 +34,22 @@
     	  </tr>
       </thead>
       <tbody>
-    	   <tr>
-       	  <td>First Serial Number</td>
-       	  <td>First Manufacturer</td>
-       	  <td>First Model</td>
-       	  <td>First Purchase Year</td>
-       	  <td>Yes</td>
-    	  </tr>
-    	  <tr style="background: #999999;"> <!-- desktop rows should have gray background-->
-      	  <td>Second Serial Number</td>
-      	  <td>Second Manufacturer</td>
-      	  <td>Second Model</td>
-      	  <td>Second Purchase Year</td>
-      	  <td>No</td>
-    	  </tr>
+  	     <?php
+  	      foreach($comp as $computers){
+  	        $color = $computers["laptop"] == "Yes" ? 'white' : '#999999';
+  	        echo "<tr style=\"background: $color\">";
+           	  echo "<td>$computers[serialNumber]</td>";
+           	  echo "<td>$computers[manufacturer]</td>";
+           	  echo "<td>$computers[model]</td>";
+           	  echo "<td>$computers[purhaseYear]</td>";
+           	  echo "<td>$computers[laptop]</td>";
+         	  echo "</tr>";
+  	      }
+     	  ?>
       </tbody>
     </table>
 
-    <p>Total Apple: ## here</p>
-    <p>Total Dell: ## here</p>
-
+    <p>Total Apple: <?php echo $appleCount; ?></p>
+    <p>Total Dell: <?php echo $dellCount; ?></p>
   </body>
 </html>
