eb3104dd09837967b6974e413c72eb11e64041ed
keita_nonaka <nonakkx16@juniata.edu>
2018-11-03 01:10:59 +0000

    minor fixes

--- ComputerList/auth/login.php
+++ ComputerList/auth/login.php
@@ -1,6 +1,11 @@
 <?php
   session_start();
 
+  if(isset($_SESSION["username"])) {
+    header('Location: ../computerList.php');
+    exit();
+  }
+
   $error_msg = '';
 
   require('../config.php');
--- ComputerList/delete.php
+++ ComputerList/delete.php
@@ -2,7 +2,7 @@
   session_start();
 
   if(!isset($_SESSION["username"])) {
-    header('Location: ./auth/login.php.php');
+    header('Location: ./auth/login.php');
     exit();
   }
 
